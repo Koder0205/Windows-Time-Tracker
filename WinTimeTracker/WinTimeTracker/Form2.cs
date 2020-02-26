bd5e7c9b4ba7462febcbad66a49d01cf4da6ea5c
Koder <koder0205@gmail.com>
2020-02-26 16:27:21 +0900

    Revert "오전 업데이트"
    This reverts commit 33f775448b26b1be75b099d31657bfa6482987f9.

--- WinTimeTracker/WinTimeTracker/Form2.cs
+++ WinTimeTracker/WinTimeTracker/Form2.cs
@@ -3,8 +3,6 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
-using System.Drawing.Text;
-using System.Runtime.InteropServices;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,35 +12,20 @@ namespace WinTimeTracker
 {
     public partial class ProgramSelect : Form
     {
-        #region LabelコントロールのDrawing Region 
-        [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]            //Dll임포트
-        private static extern IntPtr CreateRoundRectRgn                            //파라미터
-        (
-            int nLeftRect,      // x-coordinate of upper-left corner
-            int nTopRect,       // y-coordinate of upper-left corner
-            int nRightRect,     // x-coordinate of lower-right corner
-            int nBottomRect,    // y-coordinate of lower-right corner   
-            int nWidthEllipse,  // height of ellipse
-            int nHeightEllipse  // width of ellipse  
-        );
-        #endregion
-
-
         public ProgramSelect()
         {
             InitializeComponent();
             // 각 콤보박스에 데이타를 초기화
-            //comboBox1.Items.Insert(0, "==프로그램을 선택해 주세요==");
-            //comboBox2.Items.Insert(0, "==태그를 선택해 주세요==");
+            comboBox1.Items.Insert(0, "==프로그램을 선택해 주세요==");
+            comboBox2.Items.Insert(0, "==태그를 선택해 주세요==");
             // 처음 선택값 지정. 첫째 아이템 선택
-            //comboBox1.SelectedIndex = 0;
-            //comboBox2.SelectedIndex = 0;
-            panel1.Region = Region.FromHrgn(CreateRoundRectRgn(0, 0, panel1.Width, panel1.Height, 20, 20));
+            comboBox1.SelectedIndex = 0;
+            comboBox2.SelectedIndex = 0;
         }
 
         private void pictureBox1_Click(object sender, EventArgs e)
         {
-
+            this.Close();
         }
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
@@ -71,21 +54,6 @@ namespace WinTimeTracker
         }
 
         private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label1_Click_1(object sender, EventArgs e)
-        {
-
-        }
-
-        private void pictureBox1_Click_1(object sender, EventArgs e)
-        {
-            this.Close();
-        }
-
-        private void pictureBox4_Click(object sender, EventArgs e)
         {
 
         }
